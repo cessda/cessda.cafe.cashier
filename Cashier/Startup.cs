1a354675df2fcf1ccd7301bd5748604213da6c66
Matthew Morris <matthew.morris@cessda.eu>
2019-09-19 13:25:28 +0200

    Remove custom healthcheck code

--- Cashier/Startup.cs
+++ Cashier/Startup.cs
@@ -73,10 +73,7 @@ namespace Cashier
         /// <param name="env">Hosting Environment</param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseHealthChecks("/healthcheck", new HealthCheckOptions()
-            {
-                ResponseWriter = WriteResponse
-            });
+            app.UseHealthChecks("/healthcheck");
 
             if (env.IsDevelopment())
             {
@@ -88,23 +85,6 @@ namespace Cashier
             app.UseSwaggerUi3();
 
             app.UseMvc();
-        }
-
-
-        private static Task WriteResponse(HttpContext httpContext, HealthReport result)
-        {
-            httpContext.Response.ContentType = "application/json";
-
-            var json = new JObject(
-                new JProperty("status", result.Status.ToString()),
-                new JProperty("results", new JObject(result.Entries.Select(pair =>
-                    new JProperty(pair.Key, new JObject(
-                        new JProperty("status", pair.Value.Status.ToString()),
-                        new JProperty("description", pair.Value.Description),
-                        new JProperty("data", new JObject(pair.Value.Data.Select(
-                            p => new JProperty(p.Key, p.Value))))))))));
-            return httpContext.Response.WriteAsync(
-                json.ToString(Formatting.Indented));
         }
 
         private const string _inMemDatabase = "coffee-db";
